3d5f96ed5829bb0b1afe5dfa0570773da5fea47b
tessfallon <tessfallon@dhcp-10-29-192-93.dyn.MIT.EDU>
2024-04-09 12:45:35 -0400

    edits-to-diffScore

--- code/diffScore.py
+++ code/diffScore.py
@@ -20,148 +20,81 @@ import random
 class diffScore:
     
     # initialize in constructor 
-    essential = None
-    nonessential = None
+  
     test = None 
-    
-    # other parameters, optional to change 
-    metric = 'b'
-    threshold = 0.5
-    weight_e = 2
-    weight_n = 0
+    gene_list = None
+    genes_to_test = None 
+
     
     # define later 
     p = None
     score = None 
-    essential_misses = None
-    essential_hits = None 
+
     
-    def __init__(self, test, essential, nonessential, 
-                 metric = 'b',threshold=0.5,weight_e=2, weight_n=0.0):
+    def __init__(self, test, gene_list):
         '''
         Initializes the class instance, takes pandas dataframes. 
-        test = dataframe with the list of log2fold of TEST vs. PRIMARY for each gene 
-        essential = dataframe of essential genes determined via pipeline 
-        nonessential = dataframe of nonessential genes determined via pipeline 
-        metric = 'b' or 'g' for binary or graded matrix for calculating hit and miss scores  
-            default = 'b' 
-        threshold = threshold for log2fold change to be considered differentially expressed
-            default = 0.5 
-        weight_e = weight for essential gene
-            default = 2
-        weight_n = weight for nonessential gene 
-            default = 0.5
+        test = dataframe with the list of log2fold of PRIMARY vs. TEST for each gene 
+        gene_list = dataframe with genes determined to be important for CM function
         '''
         self.test = test 
-        self.essential = essential
-        self.nonessential = nonessential
-        self.metric = metric 
-        self.threshold = threshold
-        self.weight_e = weight_e
-        self.weight_n = weight_n
-    
+        self.gene_list= gene_list
+        self.genes_to_test = self.test[self.test['Gene'].isin(self.gene_list['Gene'])]
+   
         
     # gets score based on metric initialized during object initialization  
     def score(self):
-        if(self.metric == 'b'):
-            return {'Score': self.__binary(self.test), 'p':self.__get_p()}
+        self.score = self.__score(self.test)
+        return {'Score': self.score, 'p':self.__get_p()}
         
-        else:
-            return {'Score': self.__graded(self.test), 'p':self.__get_p()}
+    
         
     # local function for binary weighting 
-    def __binary(self, test):
+    def __score(self, test):
         ''' 
         Returns the differentiation score for the initiated class. 
         Defines alignment based on global threshold for log2foldchange and a p<0.05 
         Treats each "hit/miss" as the same no matter the log2 fold change/FDR 
         '''
         
-        diff = test[(abs(test['log2FoldChange']) >= self.threshold) & 
-                            (test['pvalue'] <= 0.05)]
-        
-        aligned = test[(abs(test['log2FoldChange']) < self.threshold) |
-                            (test['pvalue'] > 0.05)]
-        
-        # get hits and misses of essential and nonessential genes 
-        hits_e = aligned[aligned['genes'].isin(self.essential['genes'])]
-        hits_n = aligned[aligned['genes'].isin(self.nonessential['genes'])]
-
-        miss_e = diff[diff['genes'].isin(self.essential['genes'])]
-        miss_n = diff[diff['genes'].isin(self.nonessential['genes'])]
-        
-        
-        hits = len(hits_e)*self.weight_e + len(hits_n)*self.weight_n
-        # do we want to weight same as hits, or over-weight if there is a miss of an essential gene?
-        misses = len(miss_e)*self.weight_e - len(miss_n)*self.weight_n
-        
-        # store as global variables 
-        self.essential_hits = hits_e
-        self.essential_misses = miss_e 
-        
-        # store essential gene hits/misses as global variables 
-        return hits / misses 
-        
-   
-
-    # local function for graded weighting 
-    def __graded(self,test):
-        ''' 
-        Returns the differentiation score for the initiated class. 
-        Defines alignment based on global threshold for log2foldchange and a p<0.05 
-        Treats each "hit/miss" differently depending on the log2fold change and FDR of the gene 
-        '''
-        diff = test[(abs(test['log2FoldChange']) >= self.threshold) & 
-                           (test['pvalue'] <= 0.05)]
-       
-        aligned = test[(abs(test['log2FoldChange']) < self.threshold) |
-                           (test['pvalue'] > 0.05)]
-        
-        # get hits and misses of essential and nonessential genes 
-        hits_e = aligned[aligned['genes'].isin(self.essential['genes'])]
-        hits_n = aligned[aligned['genes'].isin(self.nonessential['genes'])]
-
-        miss_e = diff[diff['genes'].isin(self.essential['genes'])]
-        miss_n = diff[diff['genes'].isin(self.nonessential['genes'])]
+        total = 0
+        n = 0
+        for i in range(0,len(self.gene_list)):
+            gene = self.gene_list.iloc[i, 0]
+            gene_test = test[test['Gene']==gene]
+            # in case gene is not in test case 
+            #print(gene_test)
+            if(len(gene_test)>0):
+                total += 1/max(1,abs(gene_test['log2FoldChange'].to_numpy()[0]))
+                n+=1
+            
+        return total/n
         
         
-        hits = 0
-        misses = 0
-        for gene in hits_e:
-            hits += gene['log2FoldChange']*(1-gene['Product'])*self.weight_e
-        for gene in hits_n:
-            hits += gene['log2FoldChange']*(1 - gene['Product'])*self.weight_n
-        for gene in miss_e:
-            misses += gene['log2FoldChange']*(gene['Product'])*self.weight_e
-        for gene in miss_n:
-            misses += gene['log2FoldChange']*(gene['Product'])*self.weight_n
-        
-        
-        # store essential gene hits/misses as global variables 
-        self.essential_hits = hits_e
-        self.essential_misses = miss_e 
-        
-        return hits / misses 
-    
-    
     
     def __get_p(self):
         '''
         Runs 1000 trials, permutes gene names across whole dataset. 
         '''
-        vals = np.zeros(1000)
-        for i in len(0,1000):
+        vals = np.zeros(100)
+        
+        for i in range(0,100):
             vals[i] = self.__run_null()
     
         # only get values higher than score 
         more_extreme = [x for x in vals if x >= self.score] 
         toRet = len(more_extreme)/len(vals)
+    
         
-        if(toRet == 0 or toRet == 1): 
-            self.p = 0.001
+        if(abs(toRet - 0.0) < 1e-9): 
+            self.p = 0.01
         else:
             self.p = toRet 
+        
+        return self.p
     
+    def __float_equals(float1, float2, tolerance=1e-9):
+        return abs(float1 - float2) < tolerance
     
     def __run_null(self):
         '''
@@ -170,18 +103,33 @@ class diffScore:
         '''
         
         copy = self.test.copy()
-        genes = sorted(self.test['genes'], key=lambda k: random.random()) # shuffle 
-        copy['genes'] = genes
-        if(self.metric == 'b'):
-            return self.__binary(copy)
-        else:
-            return self.__graded(copy)
+        genes = sorted(self.test['Gene'], key=lambda k: random.random()) # shuffle 
+        copy['Gene'] = genes
+        
+        return self.__score(copy)
+        
         
         
 if __name__ == '__main__':
     # must run get_essential_genes.py first 
-    essential = pd.read_table('../data/essential_CM.tsv')
-    nonessential = pd.read_table('../data/unessential_CM.tsv')
+    #test = pd.read_table('../data/log_output_fibroblast.tsv',sep='\t')
+    test = pd.read_table('../data/log_output_cm.tsv',sep='\t')
+    #test = pd.read_table('../data/log_output_lung.tsv',sep='\t')
+    # needs to be the same format! 
+    test.rename(columns={'Gene Name': 'Gene'}, inplace=True)
+    
+   #  test = pd.read_table('../data/primary_goodDiff.tsv',sep='\t')
+    gene_list = pd.read_table('../data/essential_CM.tsv',sep='\t')
+    gene_list = gene_list.iloc[:,1:]
+    
+    
+    s = diffScore(test,gene_list)
+    
+    score = s.score()
+    print(score)
+    
+
+    
     
     
     
--- code/get_essential_genes.py
+++ code/get_essential_genes.py
@@ -15,21 +15,49 @@ Finds the subpopulation of genes that are cardiac-specific and necessary for goo
 
 import pandas as pd
 
+'''
 # run once to split up data into essential and nonessential genes, save as .tsv files  
-  
 
 # read in datafiles 
 og_primary = pd.read_table('../data/DE_og_primary.tsv')
-goodDiff_primary = pd.read_table('./data/DE_allDiff_primary.tsv')
+goodDiff_primary = pd.read_table('../data/DE_allDiff_primary.tsv')
 goodDiff_badDiff = pd.read_table('../data/DE_bad_good.tsv')
 
+# genes needed for good cardiac differentiation 
+diff_good = goodDiff_badDiff[((goodDiff_badDiff['log2FoldChange'] >= 0.5) & 
+                         (goodDiff_badDiff['-log10FDR'] >= 1.31))]
+
+
+
+
+# genes not needed--> cutoff 2 or 5 depending on title of file 
+unessential = goodDiff_primary[((goodDiff_primary['log2FoldChange'] <= -2) & 
+                         (goodDiff_primary['-log10FDR'] >= 1.31))]
+
+unessential =  unessential[unessential['Gene'].isin(diff_good['Gene'])]
+
+
+# cm essential genes 
+essential =  diff_good[diff_good['Gene'].isin(unessential['Gene']) == False]
+
+
+essential.to_csv('../data/essential_CM_2.tsv',sep='\t')
+unessential.to_csv('../data/unessential_CM_2.tsv',sep='\t')
+
+
+'''
+
+# read in datafiles 
+og_primary = pd.read_table('../data/DE_og_primary_new.tsv')
+goodDiff_primary = pd.read_table('../data/primary_goodDiff.tsv')
+goodDiff_badDiff = pd.read_table('../data/DE_bad_good.tsv')
 
 # CM-specific genes are differentially expressed in primary and not og 
-cm_specific = og_primary[((abs(og_primary['log2FoldChange']) >= 0.5) & 
+cm_specific = og_primary[((og_primary['log2FoldChange'] >= 0.5) & 
                          (og_primary['-log10FDR'] >= 1.31))]   # 1.3 is -log10(0.05)
 
 # genes needed for cardiac differentiation 
-diff_good = goodDiff_badDiff[((abs(goodDiff_badDiff['log2FoldChange']) >= 0.5) & 
+diff_good = goodDiff_badDiff[((goodDiff_badDiff['log2FoldChange'] >= 0.5) & 
                          (goodDiff_badDiff['-log10FDR'] >= 1.31))]
 
 
@@ -37,9 +65,8 @@ diff_good = goodDiff_badDiff[((abs(goodDiff_badDiff['log2FoldChange']) >= 0.5) &
 cm_needed = cm_specific[cm_specific['Gene'].isin(diff_good['Gene'])]
 
 
-
 # genes not needed
-unessential = goodDiff_primary[((abs(goodDiff_primary['log2FoldChange']) >= 2) & 
+unessential = goodDiff_primary[((goodDiff_primary['log2FoldChange'] <= -2) & 
                          (goodDiff_primary['-log10FDR'] >= 1.31))]
 
 unessential =  unessential[unessential['Gene'].isin(cm_needed['Gene'])]
@@ -49,5 +76,13 @@ unessential =  unessential[unessential['Gene'].isin(cm_needed['Gene'])]
 essential =  cm_needed[cm_needed['Gene'].isin(unessential['Gene']) == False]
 
 
+
 essential.to_csv('../data/essential_CM.tsv',sep='\t')
 unessential.to_csv('../data/unessential_CM.tsv',sep='\t')
+
+
+
+
+
+
+
